1945612a45647a03d81934a1c8c8b7a544db1c9f
Ayush Bhardwaj <ayush.ds321@gmail.com>
2025-07-07 23:24:05 +0530

    auth-setup

---
+++ src/auth.ts
@@ -0,0 +1,26 @@
+import NextAuth from 'next-auth';
+import {PrismaAdapter} from '@auth/prisma-adapter'
+import {GithubProvider} from 'next-auth/providers/github';
+import {prisma} from '@/lib';
+
+if(!process.env.GITHUB_CLIENT_ID || !process.env.GITHUB_CLIENT_SECRET) {
+    throw new Error('GITHUB_CLIENT_ID and GITHUB_CLIENT_SECRET must be set in .env.local');
+}
+export const {handlers:{GET,POST},auth}=NextAuth({
+    adapter:PrismaAdapter(prisma),
+    providers:[
+        GithubProvider({
+            clientId:process.env.GITHUB_CLIENT_ID,
+            clientSecret:process.env.GITHUB_CLIENT_SECRET
+        })
+    ],
+    callbacks:{
+        async session({user,session}){
+            if(session&&user){
+                session.user.id=user.id
+            }
+            return session;
+        }
+        
+    }
+})
