b9c02d8b1313e7f928826256b267869efa0bdb21
Ayush Bhardwaj <ayush.ds321@gmail.com>
2025-07-21 00:27:13 +0530

    nested comments

--- src/components/comments/comment-show.tsx
+++ src/components/comments/comment-show.tsx
@@ -1,31 +1,41 @@
-import { fetchCommentBYPostID } from '@/lib/query/comment'
-import React from 'react'
-import { Avatar, AvatarImage } from '../ui/avatar'
-import { AvatarFallback } from '@radix-ui/react-avatar'
-type commentShowProps={
-    postId:string,
-    commentId:string
-}
-const CommentShow:React.FC<commentShowProps> = async ({commentId,postId}) => {
-    const comments = await fetchCommentBYPostID(postId)
-    const comment=comments.find((c)=>c.id===commentId);
-    if(!comment) return null;
-    const childrenComment=comments.filter((c)=>c.parentId=commentId)
+import { fetchCommentBYPostID } from "@/lib/query/comment";
+import React from "react";
+import { Avatar, AvatarImage,AvatarFallback } from "../ui/avatar";
+import CommentCreateForm from "./comment-create-form";
+type commentShowProps = {
+  postId: string;
+  commentId: string;
+};
+const CommentShow: React.FC<commentShowProps> = async ({
+  commentId,
+  postId,
+}) => {
+  const comments = await fetchCommentBYPostID(postId);
+  const comment = comments.find((c) => c.id === commentId);
+  if (!comment) return null;
+  const childrenComment = comments.filter((c) => (c.parentId === commentId));
   return (
-    <div>
-        <div>
-            <Avatar>
-                <AvatarImage src={comment.user.image||""}/>
-                <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+    <div className="m-4 p-4 border rounded-xl">
+      <div className="flex gap-3">
+        <Avatar>
+          <AvatarImage src={comment.user.image || ""} />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <div className="flex-1 space-y-3">
+          <p className="text-gray-200 font-semibold">{comment.user.name}</p>
+          <p className="text-gray-300">{comment.content}</p>
+          <CommentCreateForm postId={postId} parentId={comment.id} />
         </div>
-        {
-            childrenComment.map((comment)=>(
-                <CommentShow key={comment.id} postId={comment.postId} commentId={comment.id}/>
-            ))
-        }
+      </div>
+      {childrenComment.map((comment) => (
+        <CommentShow
+          key={comment.id}
+          postId={postId}
+          commentId={comment.id}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default CommentShow
+export default CommentShow;
